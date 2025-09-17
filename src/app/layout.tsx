91c35903e7a288f7b422f4bc092e7a141ade7799
almsomnia <rivaalms@proton.me>
2025-09-17 21:42:58 +0700

    chore: add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "../assets/css/globals.css"
 import AppHeader from "@/components/app/AppHeader"
@@ -39,6 +40,7 @@ export default function RootLayout({
                <AppFooter />
             </div>
             <Analytics />
+            <SpeedInsights />
          </body>
       </html>
    )
